daeeaa033e39f4e782e31a7f7eb2392ec46bb59e
Ponraja17 <pponraja.rthoughts@gmail.com>
2022-05-13 16:12:52 +0530

    First

---
+++ math.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+   
+    printf("SQRT  %f",sqrt(4));
+    printf("\npow  %f",pow(2,3));
+    printf("\nabs  %d",abs(-44));
+    return 0;
+}
